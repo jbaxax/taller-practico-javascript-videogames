1435427185d4f197c03cfa0072a275c5e8f1d5f0
Walter Jave <walterjave92@gmail.com>
2023-10-01 13:36:07 -0500

    clase n° 5

---
+++ game.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = (canvasSize / 10)-2;
+
+  startGame();
+}
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = '';
+
+  for (let col = 0; col < 10; col++) {
+    for (let row = 1; row < 11; row++) {
+        game.fillText(emojis['X'],elementsSize*col,elementsSize*row)       
+    }
+  }
+}
